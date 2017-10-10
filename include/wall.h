aa29ee401527ccbd9c0e76f573d314eeb18985d2
destrovel <destrovel.arriba@gmail.com>
2017-10-09 22:53:29 -0600

    actually commit new files from last
    whoops

---
+++ include/wall.h
@@ -0,0 +1,28 @@
+#ifndef WALL_H
+#define WALL_H
+#include "entity.h"
+#include "texture_manager.h"
+class wall : public entity
+{
+    public:
+    enum type
+    {
+        long_wall,
+        short_wall,
+    };
+        wall(type wtype, const texture_manager& textures, float rot);
+        virtual void update_current(sf::Time delta, command_queue& cmds);
+        virtual void draw_current(sf::RenderTarget& target,
+								  sf::RenderStates  states) const;
+        virtual unsigned int get_category() const;
+        virtual ~wall();
+        virtual sf::FloatRect getBoundingRect() const;
+    protected:
+
+    private:
+    float rotation;
+    sf::Sprite sprite;
+    type wall_type;
+};
+
+#endif // WALL_H
---
+++ src/wall.cpp
@@ -0,0 +1,68 @@
+#include "wall.h"
+textures::ID to_texture_ID(wall::type ttype)
+{
+    switch (ttype)
+    {
+        case (wall::type::long_wall): return textures::long_wall;
+        case (wall::type::short_wall): return textures::short_wall;
+    }
+}
+wall::wall(type wtype, const texture_manager& textures, float rot) : wall_type(wtype),
+            sprite(textures.get(to_texture_ID(wtype))),
+            entity(1),
+            rotation(rot)
+{
+    sf::FloatRect bounds = 	sprite.getLocalBounds();
+	sprite.setOrigin(bounds.width / 2.f, bounds.height / 2.f);
+	sprite.setScale(0.5f, 0.5f);
+	sprite.setRotation(rotation);
+}
+void wall::draw_current(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::P))
+    {
+        sf::Vector2f v(getBoundingRect().width, getBoundingRect().height);
+        sf::RectangleShape collide_rect(v);
+        sf::FloatRect bounds = 	collide_rect.getLocalBounds();
+        collide_rect.setOrigin(bounds.width / 2.f, bounds.height / 2.f);
+        collide_rect.setPosition(getPosition());
+        collide_rect.setOutlineColor(sf::Color::Black);
+        collide_rect.setFillColor(sf::Color(0,0,0,0));
+        collide_rect.setOutlineThickness(.5f);
+        target.draw(collide_rect);
+    }
+    target.draw(sprite, states);
+}
+unsigned int wall::get_category() const
+{
+    switch (wall_type)
+    {
+        case long_wall:
+        case short_wall:
+            return cmd_category::walls;
+        default:
+            //lmao
+            assert(false);
+    }
+}
+void wall::update_current(sf::Time delta, command_queue& cmds)
+{
+    set_velocity(0.f, 0.f);
+}
+sf::FloatRect wall::getBoundingRect() const
+{
+    sf::FloatRect to_return(getWorldTransform().transformRect(sprite.getGlobalBounds()));
+    //to_return.left = getPosition().x;
+    //to_return.top = getPosition().y;
+    //to_return.width = sprite.getTexture()->getSize().x;
+    //to_return.height = sprite.getTexture()->getSize().y;
+    to_return.left -= 1;
+    to_return.top -= 1;
+    to_return.width += 2;
+    to_return.height += 2;
+    return to_return;
+}
+wall::~wall()
+{
+    //dtor
+}
